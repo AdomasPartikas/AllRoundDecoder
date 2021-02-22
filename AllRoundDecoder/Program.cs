8d10bfc425fe47bd55c9a342672bd046f0dcbaf2
Patrikas Adomaitis <ado.patrikas@gmail.com>
2021-02-22 12:28:52 +0200

    Pridejau pirmaja funkcija, kurios darbas nuskaityti koda kuri mes norim dekoduoti.

--- AllRoundDecoder/Program.cs
+++ AllRoundDecoder/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace AllRoundDecoder
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string inputText = "";
+            IntroText(inputText);
+        }
+
+        private static void IntroText(string code)
+        {
+            Console.WriteLine("Welcome to decipher bot.");
+            Console.Write("Please input a text: ");
+            code = Console.ReadLine();
+            Console.WriteLine(code);
+            Console.ReadKey();
         }
     }
 }
