e3b55455481e74a6daaaa80f54f04adc9af889ef
Patrikas Adomaitis <ado.patrikas@gmail.com>
2021-02-25 13:20:02 +0200

    Added a word descrambler that tries to make word combinations readable.

--- AllRoundDecoder/Program.cs
+++ AllRoundDecoder/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.Cryptography;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace AllRoundDecoder
 {
@@ -11,10 +14,17 @@ namespace AllRoundDecoder
     {
         private static Dictionary<string, char> _morseAlphabet;
         public static string inputText;
+        public static int permutations = 0;
+        public static int correctPermutations = 0,xTemp=0;
+        public List<string> correctWordList = new List<string>();
+        public List<string> similarWords = new List<string>();
+        DateTime timeStarted;
+
+        Microsoft.Office.Interop.Word.Application appWord =
+            new Microsoft.Office.Interop.Word.Application();
         public static void Main(string[] args)
         {
             Program p = new Program();
-            p.StartingLines();
             p.Brain();
         }
 
@@ -28,10 +38,117 @@ namespace AllRoundDecoder
 
         private void Brain()
         {
-            Console.WriteLine(Morse());
-            Console.ReadLine();
+            StartingLines();
+            Console.WriteLine(ScramblerTest());
+            //Console.ReadKey();
+            //if (IsWordCorrect(ScramblerTest()))
+            //    Console.WriteLine("correct");
+            //else
+            //    Console.WriteLine("incorrect");
+            //if (IsWordCorrect(inputText))
+            //    Console.WriteLine("correct");
+            //else
+            //    Console.WriteLine("incorrect");
+            //Console.ReadKey();
+            Console.WriteLine();
+            StartingLines();
+            WordDescrambler(inputText);
+            Console.ReadKey();
         }
 
+
+        private string ScramblerTest()
+        {
+            char[] charArray = inputText.ToArray();
+            Random r = new Random(300);
+            char temp;
+            int index;
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                index = r.Next(0, charArray.Length);
+                temp = charArray[index];
+                charArray[index] = charArray[i];
+                charArray[i] = temp;
+            }
+            string finished = new string(charArray);
+
+
+            return finished;
+        }
+        private void WordDescrambler(string word)
+        {
+            //char[] arr = word.ToArray();
+            timeStarted = DateTime.Now;
+            int max = word.Length;
+            correctPermutations = 0;
+            xTemp=0;
+            DisplayWordDescrambler();
+            Dive("", 0, word, max);
+
+
+            Console.WriteLine("Words created. - {0}", DateTime.Now.ToString());
+            Console.WriteLine("Time passed: {0}s", DateTime.Now.Subtract(timeStarted).TotalSeconds);
+
+            //foreach (var item in correctWordList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine("correct permutations: " + $"{correctPermutations}");
+            Console.WriteLine("total permutations: " + $"{permutations}");
+
+
+            Console.ReadKey();
+        }
+        private void Dive(string prefix, int level, string ValidChars, int max)
+        {
+            level += 1;
+            foreach (char c in ValidChars)
+            {
+                permutations++;
+                if ((prefix + c).Length == max)
+                {
+                    Console.WriteLine(prefix + c);
+                    if (IsWordCorrect(prefix + c))
+                    {
+                        if (!correctWordList.Contains(prefix + c))
+                        {
+                           correctWordList.Add(prefix + c);
+                        }
+                        correctPermutations++;
+                        //DisplayWordDescrambler();
+                    }
+                }
+                if (permutations==xTemp+100)
+                {
+                    xTemp += 100;
+                    //DisplayWordDescrambler();
+                }
+                if (level < max)
+                {
+                    Dive(prefix + c, level, ValidChars, max);
+                }
+            }
+        }
+        private void DisplayWordDescrambler()
+        {
+            Console.Clear();
+            Console.WriteLine("Working............ - {0}", timeStarted.ToString());
+            Console.WriteLine("Correct permutations: " + $"{correctPermutations}");
+            Console.WriteLine("Total permutations: " + $"{permutations}");
+            Console.WriteLine();
+            Console.WriteLine("Words created: ");
+
+            foreach (var item in correctWordList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        private bool IsWordCorrect(string word)
+        {
+            return appWord.CheckSpelling(word);
+        }
         private string Morse()
         {
             var c = inputText.Split(' ').ToList();
@@ -48,12 +165,12 @@ namespace AllRoundDecoder
             StringBuilder output = new StringBuilder();
             foreach (string item in c)
             {
-                    if (_morseAlphabet.ContainsKey(item))
-                        output.Append(_morseAlphabet[item]);
-                    else if (item == "/" || item == "|")
-                        output.Append(" ");
+                if (_morseAlphabet.ContainsKey(item))
+                    output.Append(_morseAlphabet[item]);
+                else if (item == "/" || item == "|")
+                    output.Append(" ");
             }
-            
+
             return (output.ToString());
 
         }
